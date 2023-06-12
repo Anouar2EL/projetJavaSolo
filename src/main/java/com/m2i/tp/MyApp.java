38eb74f2e6dee27384a0df7b666f2a4b9640903c
Anouar EL KADIOUI EL IDRISSI <anouarelidrissi1996@gmai.com>
2023-06-12 11:51:02 +0200

    modif_Bonjour le monde

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
+		System.out.println("hello world , Bonjour le monde ");
 		
 
 	}
